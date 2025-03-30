83018e2619fded7783b3ab75770be3c42483d5c3
Dennis Iversen <dennis.iversen@gmail.com>
2025-03-30 15:01:59 +0200

    better config-dist

--- ollama_client/config-dist.py
+++ ollama_client/config-dist.py
@@ -17,66 +17,74 @@ DEFAULT_MODEL = "deepseek-r1:14b"
 
 # Logging
 LOG_LEVEL = logging.INFO
+
+# Reloading when code changes
 RELOAD = True
 
 # Data path for logs and database
 DATA_DIR = "data"
 DATABASE = Path(DATA_DIR) / Path("database.db")
 
-# A docker image that can be used to execute python code safely
+# Used when sending emails
+HOSTNAME_WITH_SCHEME = "https://home.10kilobyte.com"
+SITE_NAME = "home.10kilobyte.com"
+
+# NOTE: Session secret key. Change this to a more random string
+SECRET_KEY = "SOME_SECRET_KEY"
+
+# A docker image that can be used to execute python code in a secure environment
+# See: https://github.com/diversen/secure-python
 PYTHON_EXEC_TEMPLATE = (
     "docker run --network none --init --rm --memory=256m --memory-swap=256m "
     "--cpus='0.5' --ulimit nproc=2000:2000 --ulimit stack=67108864 "
     "-v {filename}:/sandbox/script.py secure-python script.py"
 )
 
-# Very simple PYTHON_EXEC_TEMPLATE but unasafe
-# PYTHON_EXEC_TEMPLATE = "python3 {filename}"
-
-# Not enabled
-PYTHON_EXEC_TEMPLATE = ""
+# NOTE: This is not a secure way to run arbitrary python code
+PYTHON_EXEC_TEMPLATE = "python3 {filename}"
 
 # Tools that can be called from the frontend
 TOOLS_CALLBACK = {
     # this tool may be called on /tools/python
     # The tool will call the function execute in the module ollama_client.tools.python_exec
     # The result will be added to the dialog
-    "python": {
-        "module": "ollama_client.tools.python_exec",
-        "def": "execute",
-    }
+    
+    # Uncomment in order to run python code. 
+    # This is an button option when python code is generated
+    # "python": {
+    #     "module": "ollama_client.tools.python_exec",
+    #     "def": "execute",
+    # }
 }
-
-HOSTNAME_WITH_SCHEME = "http://localhost:8000"
-SITE_NAME = "localhost"
-
-SECRET_KEY = "SECRET_KEY_9E22443E1889947E8BFC31138C967"
 
 
 # Model tools configuration
+
 # In my expirence most models are not very good at handling tools
 # And also: If using tools with a model you loss the ability to stream the response
 # The response will be returned as a single response
 # But anyway here is an example of how to use tools with models
 
+# def add_two_numbers(a: int, b: int) -> str:
+#     """
+#     Add two numbers
 
-def add_two_numbers(a: int, b: int) -> str:
-    """
-    Add two numbers
+#     Args:
+#       a: The first integer number
+#       b: The second integer number
 
-    Args:
-      a: The first integer number
-      b: The second integer number
-
-    Returns:
-      str: The sum of the two numbers
-    """
-    return f"The result of adding {a} and {b} is: {a + b}"
+#     Returns:
+#       str: The sum of the two numbers
+#     """
+#     return f"The result of adding {a} and {b} is: {a + b}"
 
 
-# Models that may use tools
-# Leave empty if no models may use tools
-TOOL_MODELS = ["mistral-nemo:latest"]
+# # Models that may use ollama tools
+# # Leave empty if no models may use tools
+# TOOL_MODELS = ["mistral-nemo:latest"]
 
-# Tools available
-TOOLS_AVAILABLE = [add_two_numbers]
+# # Tools available
+# TOOLS_AVAILABLE = [add_two_numbers]
+
+TOOL_MODELS = []
+TOOLS_AVAILABLE = []
