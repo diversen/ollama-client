d461c6e6f641a99f316745253993b3dde66e5ff4
Dennis Iversen <dennis.iversen@gmail.com>
2025-04-03 21:53:37 +0200

    substitute another thinking tag

--- ollama_client/static/js/utils.js
+++ ollama_client/static/js/utils.js
@@ -4,10 +4,13 @@
  */
 function substituteThinkingTags(streamedResponseText) {
     streamedResponseText = streamedResponseText.replace(/<think>/g, '**Think begin**');
-    streamedResponseText = streamedResponseText.replace(/<\/think>/g, '**Think end**');
-
     streamedResponseText = streamedResponseText.replace(/<thinking>/g, '**Think begin**');
+    streamedResponseText = streamedResponseText.replace(/<thought>/g, '**Think begin**');
+
+    streamedResponseText = streamedResponseText.replace(/<\/think>/g, '**Think end**');
     streamedResponseText = streamedResponseText.replace(/<\/thinking>/g, '**Think end**');
+    streamedResponseText = streamedResponseText.replace(/<\/thought>/g, '**Think end**');
+    // </thought>
 
     return streamedResponseText;
 }
